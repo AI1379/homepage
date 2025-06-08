c6c39a8ae88babaa66849caaacf3a59a02a5031f
arendellian13 <listener1381@outlook.com>
2025-06-08 23:34:50 +0800

    refactor: remove unused client configuration file

--- src/.vuepress/client.ts
+++
@@ -1,8 +0,0 @@
-import "vuepress-theme-hope/presets/round-blogger-avatar.scss"
-import { defineClientConfig } from "vuepress/client";
-
-export default defineClientConfig({
-    enhance({ app, router, siteData }) { },
-    setup() { },
-    rootComponents: [],
-});
